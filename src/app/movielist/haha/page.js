4f402f40ef573154838ae51b5a92a2cc3df6d114
Olga <oachubakova@gmail.com>
2023-11-22 13:27:09 +0300

    [module_8] SSR

---
+++ src/app/movielist/haha/page.js
@@ -0,0 +1,22 @@
+import './searchForm.css';
+
+export default function SearchForm() {
+    return (
+        <>
+            <div className='SearchForm-findSection'>
+                <div className='SearchForm-container'>
+                <p className='SearchForm-title'>1FIND YOUR MOVIE</p>
+
+                    <input 
+                        className='SearchForm-input' 
+                        placeholder="What do you want to watch"
+                    />
+                    <button 
+                        className='SearchForm-button'
+                    >SEARCH
+                    </button>
+                </div>
+            </div>
+        </> 
+    );
+}
